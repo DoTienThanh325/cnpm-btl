72797c2573f01b898f09136b200db2acfcbdbb90
Thanh <dotienthanh7a2@gmail.com>
2026-05-19 23:10:20 +0700

    đã tách db đơn giản hóa cấu trúc

--- sms-app/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Compile script for SMS Java Swing application
-cd "$(dirname "$0")"
-find src -name "*.java" | xargs javac -encoding UTF-8 -cp "lib/*" -d out
-echo "Compilation complete. Run with: ./run.sh"
--- sms-app/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Run script for SMS Java Swing application
-cd "$(dirname "$0")"
-java -cp "out:lib/*" com.sms.Main
